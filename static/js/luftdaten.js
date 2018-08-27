0cfa61675012093cc57087f69e1c3e306b9b9e4f
Christophe Cauet <christophe@cauet.de>
2018-08-27 22:33:18 +0200

    Querying using secure API gateway

--- static/js/luftdaten.js
+++ static/js/luftdaten.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $.ajax({
-        url: "http://api.luftdaten.info/v1/sensor/16299/"
+        url: "https://api.luftdaten.info/v1/sensor/16299/"
     }).then(function(data) {
        $('.current-temperature').prepend(parseFloat(data[0].sensordatavalues[0].value));
        $('.current-humidity').prepend(parseFloat(data[0].sensordatavalues[1].value));
