a56cb1d193a33cbe09c99df21a4a4b73039fadd1
Christophe Cauet <christophe@cauet.de>
2018-09-18 15:45:10 +0200

    And switching array positions againg

--- static/js/luftdaten.js
+++ static/js/luftdaten.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $.ajax({
         url: "https://api.luftdaten.info/v1/sensor/16299/"
     }).then(function(data) {
-       $('#current-temperature').prepend(parseFloat(data[0].sensordatavalues[1].value));
-       $('#current-humidity').prepend(parseFloat(data[0].sensordatavalues[0].value));
+       $('#current-temperature').prepend(parseFloat(data[0].sensordatavalues[0].value));
+       $('#current-humidity').prepend(parseFloat(data[0].sensordatavalues[1].value));
     });
 });
